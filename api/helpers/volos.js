b2f8e5ca39f2228ac4d03336c310596617a90fb2
richardkdrew <richardkdrew@users.noreply.github.com>
2015-06-09 06:39:13 +1200

    Added spike arrest example

--- api/helpers/volos.js
+++ api/helpers/volos.js
@@ -1,8 +1,9 @@
 'use strict';
 
 module.exports = {
-    setQuotaKey: setQuotaKey,
-    setCacheKey: setCacheKey
+    setQuotaKey         :   setQuotaKey,
+    setCacheKey         :   setCacheKey,
+    setSpikeArrestKey   :   setSpikeArrestKey
 };
 
 function setQuotaKey(req) {
@@ -14,5 +15,11 @@ function setQuotaKey(req) {
 function setCacheKey(req) {
     var key = req.swagger.params.apiKey.value;
     console.log('Cache Key: ' + key);
+    return key;
+}
+
+function setSpikeArrestKey(req) {
+    var key = req.connection.remoteAddress;
+    console.log('Spike Arrest Key: ' + key);
     return key;
 }
